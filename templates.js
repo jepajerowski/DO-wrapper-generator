47c8fd3144f2183516623e5204138dfa55a8de64
jepajerowski <69466122+jepajerowski@users.noreply.github.com>
2023-01-17 10:45:58 -0500

    Update templates.js

--- templates.js
+++ templates.js
@@ -9,7 +9,7 @@ const templateList = [
     html: `
 <div class="text-black-gray text-decoration-none">
   <div class="quotation bg-transparent">
-    <img src="/specs/products/aaas/releasedAssets/images/quotation-mark.svg" alt="quotation mark" class="quotation__img"/>
+    <img src="/pb-assets/images/styleguide/quotation-mark.svg" alt="quotation mark" class="quotation__img"/>
     <div class="quotation__text">{{{Text}}}</div>
     <ul class="quotation__source list-inline">
       <li class="list-inline-item text-bold">
@@ -591,9 +591,10 @@ const templateList = [
     {
     name: "News Feature Paired Images (full-width)",
     fields: [
-      { name: "Image URL Left" },
+      { name: "Article DOI" },
+      { name: "Image file name Left",  note: "File names must be lowercase." },
       { name: "Alt Text Left" },
-      { name: "Image URL Right" },
+      { name: "Image file name Right", note: "File names must be lowercase." },
       { name: "Alt Text Right" },
       { name: "Figure Caption", type: "textarea" },
       { name: "Credit Text" }
@@ -602,10 +603,10 @@ const templateList = [
 <figure class="news-article__figure border-light-gray  plain border-bottom pb-3">
   <div class="news-article__figure__two-figures">
     <div class="news-article__figure__image__wrapper">
-      <img src="{{{Image URL Left}}}" alt="{{{Alt Text Left}}}" class="news-article__figure__image">
+      <img src="/do/{{{Article DOI}}}/files/{{{Image file name Left}}}" alt="{{{Alt Text Left}}}" class="news-article__figure__image">
     </div>
     <div class="news-article__figure__image__wrapper">
-      <img src="{{{Image URL Right}}}" alt="{{{Alt Text Right}}}" class="news-article__figure__image">
+      <img src="/do/{{{Article DOI}}}/files/{{{Image file name Right}}}" alt="{{{Alt Text Right}}}" class="news-article__figure__image">
     </div>
   </div>
   <figcaption class="news-article__figure__caption">
@@ -616,43 +617,52 @@ const templateList = [
   },
   {
     name: "Slideshow",
-    fields: [],
+    fields: [
+      {
+        name: "Width",
+        type: "options",
+        options: [
+          ["Column-width", " non-featured"],
+          ["Full-width (for use in News Features only)", ""],
+        ],
+        default: " non-featured"
+      }
+    ],
     child: {
       fields: [
         { name: "Article DOI" },
-      { name: "Image file name",  note: "File names must be lowercase." },
+        { name: "Image file name",  note: "File names must be lowercase." },
         { name: "Alt Text" },
         { name: "Figure Caption", type: "textarea" },
         { name: "Credit Text" }
       ],
       html: `
-        <div class="swiper-wrapper">
-          <div class="swiper-slide">
-            <div class="news-article__figure-img-wrapper">
-              <img src="/do/{{{Article DOI}}}/files/{{{Image file name}}}" alt="{{{Alt Text}}}" class="news-article__figure-img">
-              <div class="news-article__figure-caption d-none"><span class="text-sm text-gray letter-spacing-default">{{{Figure Caption}}}<span class="text-xxs text-uppercase">{{{Credit Text}}}</span></span></div>
-            </div>
+        <div class="swiper-slide">
+          <div class="news-article__figure-img-wrapper"><img class="news-article__figure-img" src="/do/{{{Article DOI}}}/files/{{{Image file name}}}" alt="{{{Alt Text}}}"/>
+            <div class="news-article__figure-caption d-none"><span class="text-sm text-gray letter-spacing-default">{{{Figure Caption}}}<span class="ml-2 text-xxs text-uppercase">{{{Credit Text}}}</span></span></div>
           </div>
         </div>
       `
     },
     html: `
-<div class="news-article__slideshow__wrapper">
-  <a href="#afterSlideshow-vlb" class="sr-only sr-only-focusable">Skip slideshow</a>
-  <div class="news-article__slideshow mb-2">
-    <div class="swiper-container-wrapper">
-      <div data-loop="true" data-items="1" data-slideby="1" data-autoheight="false" data-responsive="" data-speed="2000" data-autoplay="false" data-animation="slide" data-indicators="true" data-arrow="true" data-controls="true" data-stagepadding="0" data-label="Slideshow" class="swiper-container" data-pause-slide="Pause slideshow" data-play-slide="Play slideshow" data-go-to-slide="Go to slide:" data-next-slide="Go to next slide" data-prev-slide="Go to previous slide">
+<div class="news-article__slideshow__wrapper{{{Width}}}"><a class="sr-only sr-only-focusable" href="#afterSlideshow-shi">Skip slideshow</a>
+    <div class="news-article__slideshow mb-2">
+        <div class="swiper-container-wrapper">
+            <div class="swiper-container swiper-container-initialized swiper-container-horizontal swiper-container-pointer-events" style="cursor: grab;" data-loop="true" data-items="1" data-slideby="1" data-autoheight="false" data-responsive="" data-speed="2000" data-autoplay="false" data-animation="slide" data-indicators="true" data-arrow="true" data-controls="true" data-stagepadding="0" data-label="Slideshow" data-pause-slide="Pause slideshow" data-play-slide="Play slideshow" data-go-to-slide="Go to slide:" data-next-slide="Go to next slide" data-prev-slide="Go to previous slide" aria-roledescription="Slideshow">
+                <div id="swiper-wrapper-e307aee6496d3c8d" class="swiper-wrapper" style="transition-duration: 0ms;" aria-live="polite">
 {{{Child}}}
-      </div>
-      <div class="swiper-button-container">
-        <div class="swiper-button-prev"></div>
-        <div class="swiper-button-next"></div>
-      </div>
+                </div>
+                <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span>
+            </div>
+            <div class="swiper-button-container">
+                <div class="swiper-button-prev" tabindex="0" role="button" aria-label="Go to previous slide" aria-controls="swiper-wrapper-e307aee6496d3c8d"><wbr /></div>
+                <div class="swiper-button-next" tabindex="0" role="button" aria-label="Go to next slide" aria-controls="swiper-wrapper-e307aee6496d3c8d"><wbr /></div>
+            </div>
+        </div>
     </div>
-  </div>
-  <figcaption class="border-light-gray pb-2"></figcaption>
-  <div class="swiper-pagination"></div>
-  <span id="afterSlideshow-vlb" tabindex="-1"></span>
+    <div class="news-article__slideshow-figcaption border-light-gray pb-2">&nbsp;</div>
+    <div class="swiper-pagination swiper-pagination-clickable swiper-pagination-bullets">&nbsp;</div>
+    <span id="afterSlideshow-shi" tabindex="-1"></span>
 </div>
     `
   },
@@ -804,20 +814,11 @@ const templateList = [
 </div>
     `
   },
+ 
   {
-    name: "Video (Brightcove)",
+    name: "Video (Cadmore)",
     fields: [
       { name: "Video ID" },
-      {
-        name: "Aspect Ratio",
-        type: "options",
-        options: [
-          ["16:9", "56.25"],
-          ["9:16 (vertical)", "177.778"],
-          ["1:1 (square)", "100"]
-        ],
-        default: "56.25"
-      },
       {
         name: "Position",
         type: "options",
@@ -847,11 +848,8 @@ const templateList = [
 
 <figure class="news-article__figure border-light-gray{{{Is there a caption?}}}{{{Position}}}">
   <div class="news-article__figure__image__wrapper">
-    <div class="mb-2 news-article__figure__image" style="position: relative; display: block; max-width: 100%;">
-      <div style="padding-top: {{{Aspect Ratio}}}%;">
-    <iframe src="https://players.brightcove.net/53038991001/a7scWzO7u_default/index.html?videoId={{{Video ID}}}" allowfullscreen webkitallowfullscreen mozallowfullscreen style="position: absolute; top: 0px; right: 0px; bottom: 0px; left: 0px; width: 100%; height: 100%;">
-    </iframe>
-      </div>
+    <div class="mb-2 news-article__figure__image">
+      <div class="cmpl_iframe_div"><iframe class="cmpl_iframe" style="border: 0px; height: 100%; margin: 0px; overflow: hidden; width: 100%;" src="https://play.cadmore.media/Player/{{{Video ID}}}" allowfullscreen="allowfullscreen"></iframe></div>
     </div>
   </div>
   <figcaption class="news-article__figure__caption"><span class="text-sm text-gray letter-spacing-default">{{{Caption}}}<span class="text-xxs ml-2 text-uppercase">{{{Credit Text}}}</span></span></figcaption>
